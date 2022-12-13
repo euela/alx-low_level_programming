99d55ecbcaba6a3ed18d312c064a9d691d6e3676
euela <euela3979@gmail.com>
2022-12-13 06:42:22 -0800

    functions

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+
+/**
+ * *main.h file
+ * * containing functions prototype
+ * */
+
+void print_alphabet(void);
+void times_table();
+void print_alphabet_x10(void);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+int add(int n, int m);
+void print_to_98(int);
+int print_last_digit(int n);
+void jack_bauer(void);

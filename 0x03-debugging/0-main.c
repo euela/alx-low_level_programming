1b703e00c96ed9801f783313f05e3618843e8b5d
euela <euela3979@gmail.com>
2022-12-13 11:34:30 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*postive or negative function*/
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/2-largets_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*function larget beween the following numbers*/
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}

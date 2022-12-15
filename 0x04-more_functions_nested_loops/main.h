6f2c70ade1167f34d7f9097b0b3d8ddfdd0a7246
euela <euela3979@gmail.com>
2022-12-15 09:45:38 -0800

    functions and loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,7 +12,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_diagonal(int n);
 void print_triangle(int size);
-int _putchar(char);
+int putchar(char);
 int _isupper(int);
 void more_numbers(void);
 void print_line(int n);

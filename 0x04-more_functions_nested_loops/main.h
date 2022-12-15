c96cf0f15356d75dbaa87f521866ed9c6e91641c
euela <euela3979@gmail.com>
2022-12-15 10:55:14 -0800

    functions and loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * *main - header file for protoypes
- * *void protoypes (void)
- * *int protoypes (int)
- * */
 
-void print_square(int size);
-void print_number(int n);
+/*header file function declarations*/
+
 void print_numbers(void);
 void print_most_numbers(void);
-void print_diagonal(int n);
-void print_triangle(int size);
-int putchar(char);
-int _isupper(int);
 void more_numbers(void);
 void print_line(int n);
-int _isdigit(int);
-int mul(int, int);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */

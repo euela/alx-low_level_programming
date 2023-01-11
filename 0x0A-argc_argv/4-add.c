4b70568df7d80f5e2a633c51cb09f2222012e86a
euela <euela3979@gmail.com>
2023-01-11 05:57:10 -0800

    argc,arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,9 +22,10 @@ int main(int argc, char *argv[])
 	      printf("Error\n");
 	      return(1);
 	    }
-	  sum = sum + atoi(argv[i]);
 	}
-      printf("%d\n", sum);
-
-      return (0);
+      sum = sum + atoi(argv[i]);
     }
+  printf("%d\n", sum);
+
+  return (0);
+}

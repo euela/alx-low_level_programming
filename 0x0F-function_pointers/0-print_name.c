84990bbf2bd91da7a68f11e3fe096a0661ef9e52
euela <euela3979@gmail.com>
2023-01-28 05:56:07 -0800

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
 #include "function_pointers.h"
+#include<stdlib.h>
 /**
- *print_name: prints a name
- *@name: string mem
- *@f: function pointer
- *RETURN: void
+ * print_name - prints a name
+ * @name: person's name
+ * @f: pointer to a function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-  if(f != NULL)
+  if (f != NULL)
     f(name);
+
 }

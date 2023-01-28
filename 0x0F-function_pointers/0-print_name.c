17346ddf9540b61214c60a8ef83519a2776e813f
euela <euela3979@gmail.com>
2023-01-28 05:47:44 -0800

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#inlcude "function_pointers.h"
+/**
+ *print_name: prints a name
+ *@name: string mem
+ *@f: function pointer
+ *RETURN: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+  if(f != NULL)
+    f(name);
+  printf("%s",name);
+}
